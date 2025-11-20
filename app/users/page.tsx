6d1aa2c91bbae9e97868017b33447a906fe5d429
cyril <cyril.ndudi@gmail.com>
2025-11-19 19:59:01 -0500

    Another commit

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,6 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 import { db } from "@/db";
 import { users } from "@/db/schema";
